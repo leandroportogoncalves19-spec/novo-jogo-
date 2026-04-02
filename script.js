6c9c7573d48730083b2b6454405432367992652d
leandroportogoncalves19-spec <leandroportogoncalves19@gmail.com>
2026-04-01 21:32:05 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+ // ===== CONFIGURAÇÃO =====
 let time = 30;
 let timer = null;
 let paused = false;
+let score = 0;
 
-// ELEMENTOS
+// ===== ELEMENTOS =====
 const btnPlay = document.getElementById("btnPlay");
 const btnPause = document.getElementById("btnPause");
 const btnResume = document.getElementById("btnResume");
@@ -11,11 +13,14 @@ const btnBack = document.getElementById("btnBack");
 const btnMenu = document.getElementById("btnMenu");
 
 const timeText = document.getElementById("time");
+const scoreText = document.getElementById("score");
+const finalScoreText = document.getElementById("finalScore");
+
 const monkey = document.getElementById("monkey");
 const feedback = document.getElementById("feedback");
 const playArea = document.getElementById("playArea");
 
-// EVENTOS
+// ===== EVENTOS =====
 btnPlay.onclick = startGame;
 btnPause.onclick = pauseGame;
 btnResume.onclick = resumeGame;
@@ -23,7 +28,7 @@ btnHow.onclick = () => showScreen("how");
 btnBack.onclick = () => showScreen("menu");
 btnMenu.onclick = () => showScreen("menu");
 
-// TROCAR TELA
+// ===== TROCAR TELAS =====
 function showScreen(id) {
   document.querySelectorAll(".screen").forEach(tela => {
     tela.classList.remove("active");
@@ -32,61 +37,13 @@ function showScreen(id) {
   document.getElementById(id).classList.add("active");
 }
 
-// INICIAR JOGO
+// ===== INICIAR JOGO =====
 function startGame() {
   time = 30;
+  score = 0;
   paused = false;
+
   timeText.textContent = time;
+  scoreText.textContent = score;
 
   showScreen("game");
-  moveMonkey();
-
-  clearInterval(timer);
-
-  timer = setInterval(() => {
-    if (!paused) {
-      time--;
-      timeText.textContent = time;
-
-      if (time <= 0) {
-        endGame();
-      }
-    }
-  }, 1000);
-}
-
-// MOVER MACACO
-function moveMonkey() {
-  const areaWidth = playArea.offsetWidth;
-  const areaHeight = playArea.offsetHeight;
-
-  const x = Math.random() * (areaWidth - 100);
-  const y = Math.random() * (areaHeight - 100);
-
-  monkey.style.left = x + "px";
-  monkey.style.top = y + "px";
-}
-
-// CLIQUE
-monkey.onclick = () => {
-  feedback.textContent = "UHUU! 🎉";
-  moveMonkey();
-};
-
-// PAUSAR
-function pauseGame() {
-  paused = true;
-  showScreen("pause");
-}
-
-// VOLTAR
-function resumeGame() {
-  paused = false;
-  showScreen("game");
-}
-
-// FINAL
-function endGame() {
-  clearInterval(timer);
-  showScreen("end");
-}
